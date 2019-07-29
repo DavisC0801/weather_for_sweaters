62bc652f1ba8c0d9ead7df21ae0d38f5d01b5d63
davisc0801 <davisc1014@gmail.com>
2019-07-29 10:27:02 -0600

    Add food search params to resturant service call

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
     time_arrived = DirectionsService.find_arrival_time(params[:start], params[:end])
-    open_resturants = ResturantService.find_open_resturants(params[:end], time_arrived)
+    open_resturants = ResturantService.find_open_resturants(params[:end], time_arrived, params[:food])
     resturant_list = open_resturants.map { |attributes| Resturant.new(attributes) }
     render json: ResturautSerializer.new(resturant_list).to_json
   end
