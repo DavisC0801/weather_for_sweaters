a6c7fc241a5ff80e24b3f067b836dfad6bbcbab4
davisc0801 <davisc1014@gmail.com>
2019-07-29 11:44:15 -0600

    Add city feature test for munchies endpoint

--- spec/requests/api/v1/munchies_spec.rb
+++ spec/requests/api/v1/munchies_spec.rb
@@ -5,4 +5,10 @@ describe "/api/v1/munchies" do
     get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"
     expect(response).to be_successful
   end
+
+  it "includes a city" do
+    get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"
+    response = JSON.parse(response.body)
+    expect(response["data"]["attributes"]["city"]).to eq("Pueblo, CO")
+  end
 end
