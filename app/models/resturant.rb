a424857b81a90886e18cff956f7a5e7b61105280
davisc0801 <davisc1014@gmail.com>
2019-07-29 10:56:45 -0600

    Add attr_readers to resturant model

--- app/models/resturant.rb
+++ app/models/resturant.rb
@@ -1,6 +1,9 @@
 class Resturant
+  attr_reader :id, :name, :address
+
   def initialize(attributes)
+    @id = attributes[:id]
     @name = attributes[:name]
-    @address = attributes[:location][:display_address].join
+    @address = attributes[:location][:display_address].join(" ")
   end
 end
