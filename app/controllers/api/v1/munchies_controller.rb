c5218f739370da290e6903b6bae1eb80d8856553
davisc0801 <davisc1014@gmail.com>
2019-07-29 10:33:27 -0600

    Correct typo in controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::MunchiesController < ApplicationController
     time_arrived = DirectionsService.find_arrival_time(params[:start], params[:end])
     open_resturants = ResturantService.find_open_resturants(params[:end], time_arrived, params[:food])
     resturant_list = open_resturants.map { |attributes| Resturant.new(attributes) }
-    render json: ResturautSerializer.new(resturant_list).to_json
+    render json: ResturantSerializer.new(resturant_list).to_json
   end
 end
