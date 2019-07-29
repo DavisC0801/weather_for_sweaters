03efa0e006cb15ee8fc3368ddc7c9e40ae068ee2
davisc0801 <davisc1014@gmail.com>
2019-07-29 15:53:30 -0600

    Add limit to background service call in background controller

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
-    background = BackgroundService.find_background(params[:location])
+    background = BackgroundService.find_background_url(params[:location], 1)
     render json: BackgroundSerializer.new(Background.new(background)).to_json
   end
 end
