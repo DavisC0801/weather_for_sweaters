af5f321472206b6c5dbc487e12e02361f2d0a7b0
davisc0801 <davisc1014@gmail.com>
2019-07-28 21:31:07 -0600

    Correct route namespacing

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,7 @@
-class ForecastController < ApplicationController
+class Api::V1::ForecastController < ApplicationController
   def show
     locationdata = GeocodeService.find_coordinates(params[:location])
     forecast = ForecastService.find_forecast(locationdata[:coordinates])
-    ForecastSerializer.new(locationdata, forecast).to_json
+    render json: ForecastSerializer.new(Forecast.new(forecast)).to_json
   end
 end
