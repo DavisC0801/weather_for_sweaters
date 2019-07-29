0004607159fed0fa5ffcaff1b73b815e5886f3a2
davisc0801 <davisc1014@gmail.com>
2019-07-29 16:19:35 -0600

    Add cassette to background spec

--- spec/requests/api/v1/background_spec.rb
+++ spec/requests/api/v1/background_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 describe "/api/v1/backgrounds" do
-  it "renders a page" do
-    get "/api/v1/backgrounds?location=denver,co"
-    expect(response).to be_successful
+  VCR.use_cassette("background/denver") do
+    it "renders a page" do
+      get "/api/v1/backgrounds?location=denver,co"
+      expect(response).to be_successful
+    end
   end
 end
