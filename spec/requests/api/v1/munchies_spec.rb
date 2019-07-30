150e5f343b4eb3abb89f1bab3b04b65262ad05fe
davisc0801 <davisc1014@gmail.com>
2019-07-30 11:36:19 -0600

    Refactor test for integration

--- spec/requests/api/v1/munchies_spec.rb
+++ spec/requests/api/v1/munchies_spec.rb
@@ -4,8 +4,6 @@ describe "/api/v1/munchies" do
   before :each do
     VCR.use_cassette("resturants/pueblo_chinese_all", allow_playback_repeats: true) do
       allow(Time).to receive(:now).and_return(Time.at(1564423200))
-      all_resturant_list = ResturantService.find_open_resturants("pueblo,co", 1564423200, "chinese", 11)
-      @all_resturants = all_resturant_list.map { |attributes| Resturant.new(attributes) }
     end
   end
 
@@ -26,14 +24,21 @@ describe "/api/v1/munchies" do
 
   it "includes a resturant" do
     VCR.use_cassette("resturants/pueblo_chinese", allow_playback_repeats: true) do
+      all_resturant_list = ResturantService.find_open_resturants("pueblo,co", 1564423200, "chinese", 11)
+      all_resturants = all_resturant_list.map { |attributes| Resturant.new(attributes) }
+
       get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"
       parsed_response = JSON.parse(body)
       names = @all_resturants.map { |resturant| resturant.name }
       addresses = @all_resturants.map { |resturant| resturant.address }
       ids = @all_resturants.map { |resturant| resturant.id }
+
       expect(names.include?(parsed_response["data"]["attributes"]["resturant_list"].first["name"])).to be true
       expect(addresses.include?(parsed_response["data"]["attributes"]["resturant_list"].first["address"])).to be true
       expect(ids.include?(parsed_response["data"]["attributes"]["resturant_list"].first["id"])).to be true
+      expect(names.include?(parsed_response["data"]["attributes"]["resturant_list"].last["name"])).to be true
+      expect(addresses.include?(parsed_response["data"]["attributes"]["resturant_list"].last["address"])).to be true
+      expect(ids.include?(parsed_response["data"]["attributes"]["resturant_list"].last["id"])).to be true
     end
   end
 end
