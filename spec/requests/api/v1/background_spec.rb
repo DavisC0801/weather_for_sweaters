53e6b3d6aeee107076fa56312684a4a89ea25403
davisc0801 <davisc1014@gmail.com>
2019-07-30 16:07:18 -0600

    Add additional features tests and cassette for background endpoint

--- spec/requests/api/v1/background_spec.rb
+++ spec/requests/api/v1/background_spec.rb
@@ -1,10 +1,18 @@
 require "rails_helper"
 
 describe "/api/v1/backgrounds" do
-  VCR.use_cassette("background/denver") do
-    it "renders a page" do
+  it "renders a page" do
+    VCR.use_cassette("background/denver", allow_playback_repeats: true) do
       get "/api/v1/backgrounds?location=denver,co"
       expect(response).to be_successful
+    end
+  end
+
+  it "renders attributes" do
+    VCR.use_cassette("background/denver", allow_playback_repeats: true) do
+      get "/api/v1/backgrounds?location=denver,co"
+      parsed_response = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(parsed_response.keys).to contain_exactly(:id, :type, :attributes)
     end
   end
 end
