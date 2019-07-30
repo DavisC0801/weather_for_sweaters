438848f57a1e13948024398839722c851e5556b3
davisc0801 <davisc1014@gmail.com>
2019-07-30 17:33:57 -0600

    Add additional tests to user request spec

--- spec/requests/api/v1/account_creation_spec.rb
+++ spec/requests/api/v1/account_creation_spec.rb
@@ -7,10 +7,30 @@ describe "/api/v1/users" do
       "password": "password",
       "password_confirmation": "password"
     }
+
+    @error_params = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "notPassword"
+    }
   end
 
   it "gives a reply" do
     post "/api/v1/users", params: @params
     expect(response).to be_successful
+  end
+
+  it "returns an API key with a correct response" do
+    post "/api/v1/users", params: @params
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    new_user = User.last
+    expect(parsed_response[:api_key]).to eq(new_user.api_key)
+  end
+
+  it "returns an error with the incorrect response" do
+    post "/api/v1/users", params: @error_params
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    expect(User.last).to be_nil
+    expect(parsed_response[:error]).to eq("Password confirmation doesn't match Password")
   end
 end
