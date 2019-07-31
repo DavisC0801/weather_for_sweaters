7aaff3bf2ceae87095ed8415bbfc975cbc54cb0b
davisc0801 <davisc1014@gmail.com>
2019-07-30 19:47:43 -0600

    Add road trip models to facade and controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::RoadTripController < ApplicationController
   def show
     user = User.find_by(api_key: params[:api_key])
     if user
-      forecast = RoadTripFacade.find_destination_forecast(params[:origin], params[:destination])
-      render json: ForecastSerializer.new(forecast)
+      road_trip = RoadTripFacade.find_destination_forecast(params[:origin], params[:destination])
+      render json: RoadTripSerializer.new(road_trip)
     else
       render json: { "error" => "Invalid API Key" }, :status => 401
     end
--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -11,6 +11,6 @@ class RoadTripFacade
   def find_destination_forecast
     location_data = GoogleService.find_destination_information(@origin, @destination)
     forecast_info = ForecastService.find_future_forecast(location_data[:destination_coordinates], location_data[:arrival_time])
-    Forecast.new(forecast_info, location_data)
+    RoadTrip.new(forecast_info, location_data)
   end
 end
