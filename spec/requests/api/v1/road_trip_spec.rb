6565f2dd323068591954144d3702ad8c58f16040
davisc0801 <davisc1014@gmail.com>
2019-07-30 20:23:03 -0600

    Add tests for road trip requests

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -23,7 +23,7 @@ describe "/api/v1/road_trip" do
       "api_key": "somegarbage"
     }
   end
-  
+
   before :each do
     allow(Time).to receive(:now).and_return(Time.at(1564423200))
   end
@@ -32,6 +32,36 @@ describe "/api/v1/road_trip" do
     VCR.use_cassette("road_trip/denver_to_pueblo", allow_playback_repeats: true) do
       post "/api/v1/road_trip", params: @params
       expect(response).to be_successful
+    end
+  end
+
+  it "includes destination address info" do
+    VCR.use_cassette("road_trip/denver_to_pueblo", allow_playback_repeats: true) do
+      post "/api/v1/road_trip", params: @params
+      parsed_response = JSON.parse(response.body, symbolize_names: true)[:data][:attributes][:location]
+      expect(parsed_response.keys).to contain_exactly(:trip_duration,
+                                                      :arrival_time,
+                                                      :destination_city, 
+                                                      :destination_state,
+                                                      :destination_country)
+    end
+  end
+
+  it "includes destination weather info" do
+    VCR.use_cassette("road_trip/denver_to_pueblo", allow_playback_repeats: true) do
+      post "/api/v1/road_trip", params: @params
+      parsed_response = JSON.parse(response.body, symbolize_names: true)[:data][:attributes][:currently_detail]
+      expect(parsed_response.keys).to contain_exactly(:current_detail_summary,
+                                                      :current_detail_icon,
+                                                      :current_detail_humidity,
+                                                      :current_detail_wind_speed,
+                                                      :current_detail_wind_gust,
+                                                      :current_detail_uv_index,
+                                                      :current_detail_cloud_cover,
+                                                      :current_detail_visibility,
+                                                      :current_detail_temp,
+                                                      :current_detail_feels_like,
+                                                      :current_detail_percipitaion_chance)
     end
   end
 
