c8e90b5e2fed67dc5016cbaa50045d2f06ff403d
davisc0801 <davisc1014@gmail.com>
2019-07-30 19:33:06 -0600

    Add find future forecast method to forecast service

--- app/service/forecast_service.rb
+++ app/service/forecast_service.rb
@@ -8,8 +8,21 @@ class ForecastService
     new(location).find_forecast
   end
 
+  def self.find_future_forecast(location, arrival_time)
+    new(location).find_future_forecast(arrival_time)
+  end
+
   def find_forecast
     raw_forecast = get_json("forecast/#{ENV["DARKSKY_API_KEY"]}/#{@lat},#{@lng}", {exclude: "minutely"})
+    {
+      currently: raw_forecast[:currently],
+      hourly: raw_forecast[:hourly],
+      daily: raw_forecast[:daily]
+    }
+  end
+
+  def find_future_forecast(arrival_time)
+    raw_forecast = get_json("forecast/#{ENV["DARKSKY_API_KEY"]}/#{@lat},#{@lng},#{arrival_time}", {exclude: "minutely"})
     {
       currently: raw_forecast[:currently],
       hourly: raw_forecast[:hourly],
